465a645e6347ec5c22b1d03c727aa769498a0370
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-06-09 20:47:05 +0300

    commit tree b65dd8794367a63d66d951ce1e189dd92593e567

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+// const totalBatteries = batteryBatches.reduce(function (sum, batteryBatch) {
+//   return sum + batteryBatch;
+// });
+
+const initValue = 0;
+const totalBatteries = batteryBatches.reduce(
+  (sum, batteryBatch) => sum + batteryBatch,
+  initValue
+);
+// console.log(totalBatteries);
